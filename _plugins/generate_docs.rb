dc9893cafccc29d9266047104bda5a6ae7081481
JC Brand <jc@opkode.com>
2015-04-17 11:36:01 +0200

    Put the Patternslib docs in a 'docs' dir (not 'demo').

--- _plugins/generate_docs.rb
+++ _plugins/generate_docs.rb
@@ -7,7 +7,8 @@ require 'pathname'
 require 'diffy'
 
 module Jekyll
-  DEMO_FOLDER = 'src/bower_components/patternslib/demo'
+  PATTERNSLIB_DOCS_PATH = 'src/bower_components/patternslib/demo'
+  COPIED_DOCS_PATH = 'docs'
 
   # The Site class is a built-in Jekyll class with access to global site config information.
   class Site
@@ -16,10 +17,10 @@ module Jekyll
       # See if there is a difference between the canonical docs and the ones
       # copied to Patterns-site and if there is, copy the canonical docs over.
       dirty = false
-      if Dir.exist?('demo')
-        folders = Pathname.new(DEMO_FOLDER).children.select { |c| c.directory? }
+      if Dir.exist?(COPIED_DOCS_PATH)
+        folders = Pathname.new(PATTERNSLIB_DOCS_PATH).children.select { |c| c.directory? }
         folders.each() do |path|
-          pattern = path.sub(/^#{DEMO_FOLDER}\//, '')
+          pattern = path.sub(/^#{PATTERNSLIB_DOCS_PATH}\//, '')
 
           Dir.foreach(path) do |item|
             next if item == '.' or item == '..'
@@ -29,7 +30,7 @@ module Jekyll
             if basename.end_with?(".sw?") or basename.end_with?("~") or basename.start_with?(".")
               next
             end
-            new = "demo/#{pattern}/#{basename}"
+            new = "#{COPIED_DOCS_PATH}/#{pattern}/#{basename}"
             diff = Diffy::Diff.new(new, original, :source => 'files').to_s(:text)
             if not diff.empty?
               dirty = true
@@ -38,8 +39,8 @@ module Jekyll
           end
         end
       end
-      if (not Dir.exist?('demo')) or dirty
-        FileUtils.cp_r(DEMO_FOLDER, "./")
+      if (not Dir.exist?(COPIED_DOCS_PATH)) or dirty
+        FileUtils.cp_r(PATTERNSLIB_DOCS_PATH, COPIED_DOCS_PATH)
         puts "Copying over the documentation files from Patternslib"
       end
     end
@@ -49,15 +50,15 @@ module Jekyll
       # in the _site directory.
       patterns = []
       self.copy_docs_if_necessary()
-      folders = Pathname.new(DEMO_FOLDER).children.select { |c| c.directory? }
+      folders = Pathname.new(PATTERNSLIB_DOCS_PATH).children.select { |c| c.directory? }
       folders.each() do |path|
-        pattern_name = path.sub(/^#{DEMO_FOLDER}/, '')
+        pattern_name = path.sub(/^#{PATTERNSLIB_DOCS_PATH}/, '')
         patterns << pattern_name
 
         Dir.foreach(path) do |item|
           next if item == '.' or item == '..'
           next if not File.file?(item)
-          site.static_files << Jekyll::StaticFile.new(site, site.source, "./demo/#{pattern_name}", item)
+          site.static_files << Jekyll::StaticFile.new(site, site.source, "#{COPIED_DOCS_PATH}/#{pattern_name}", item)
         end
       end
     patterns
