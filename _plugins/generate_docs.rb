95594213411afe23bd4d50b0ca6fd6491569043c
JC Brand <jc@opkode.com>
2015-04-21 16:24:06 +0200

    In the jekyll plugin...
    recursively copy so that we can also copy subdirectories.

--- _plugins/generate_docs.rb
+++ _plugins/generate_docs.rb
@@ -49,23 +49,31 @@ module Jekyll
       end
     end
 
+    def create_static_files(site, path, pattern_name)
+      # Create Jekyll:StaticFile instances for all files that need to be copied
+      # into the _site directory.
+      Dir.foreach(path) do |item|
+        next if item == '.' or item == '..'
+        item_path = "#{path}/#{item}"
+        if File.directory?(item_path)
+          self.create_static_files(site, item_path, pattern_name)
+        else
+          rel_path = item_path.sub(PATTERNSLIB_PAT_PATH, "#{COPIED_DOCS_PATH}/patterns")
+          rel_path = rel_path.sub(item, '')
+          site.static_files << Jekyll::StaticFile.new(site, site.source, rel_path, item)
+        end
+      end
+    end
+
     def generate_docs(site)
       # Make sure that the canonical docs (e.g. from Patternslib) are available
       # in the _site directory.
-      patterns = []
       self.copy_docs_if_necessary()
       folders = Pathname.new(PATTERNSLIB_PAT_PATH).children.select { |c| c.directory? }
       folders.each() do |path|
         pattern_name = path.sub(/^#{PATTERNSLIB_PAT_PATH}/, '')
-        patterns << pattern_name
-
-        Dir.foreach(path) do |item|
-          next if item == '.' or item == '..'
-          next if not File.file?(item)
-          site.static_files << Jekyll::StaticFile.new(site, site.source, "#{COPIED_DOCS_PATH}/patterns/#{pattern_name}", item)
-        end
+        self.create_static_files(site, path, pattern_name)
       end
-    patterns
     end
   end
 
@@ -75,7 +83,7 @@ module Jekyll
     priority :low
 
     def generate(site)
-      site.generate_docs site
+      site.generate_docs(site)
     end
   end
 
