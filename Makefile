bed08db93d6fd5e9d1dca1ee698f49c6880c553a
JC Brand <jc@opkode.com>
2015-04-23 16:22:03 +0200

    Simplified the way in which we integrate Patterns repo.
    I found a way to copy over static files to _site directory while giving them a
    new relative path. This means we don't need the complicated diffing/copying
    code from the plugin anymore.
    
    Also, we now make a checkout of Patterns (via the Makefile) instead of using
    bower and linking.

--- Makefile
+++ Makefile
@@ -3,4 +3,13 @@ SASS        ?= bundle
 serve::
 	bundle exec jekyll serve  --baseurl "" --host "0.0.0.0"
 
-.PHONY: serve
+patternslib::
+	@if [ ! -d "patternslib" ]; then \
+		git clone https://github.com/Patternslib/Patterns.git patternslib; \
+		git checkout docs-refactor; \
+	 else \
+		cd patternslib && git checkout docs-refactor; git pull origin docs-refactor && cd ..; fi;
+
+designerhappy:: patternslib serve
+
+.PHONY: serve designerhappy checkout
--- _plugins/generate_docs.rb
+++ _plugins/generate_docs.rb
@@ -4,50 +4,14 @@
 require 'fileutils'
 require 'find'
 require 'pathname'
-require 'diffy'
 
 module Jekyll
-  PATTERNSLIB_DOCS_PATH = 'src/bower_components/patternslib/docs'
-  PATTERNSLIB_PAT_PATH = 'src/bower_components/patternslib/docs/patterns'
+  PATTERNSLIB_DOCS_PATH = 'patternslib/docs'
+  PATTERNSLIB_PAT_PATH = 'patternslib/docs/patterns'
   COPIED_DOCS_PATH = 'docs'
 
   # The Site class is a built-in Jekyll class with access to global site config information.
   class Site
-
-    def copy_docs_if_necessary
-      # See if there is a difference between the canonical docs and the ones
-      # copied to Patterns-site and if there is, copy the canonical docs over.
-      dirty = false
-      if Dir.exist?(COPIED_DOCS_PATH)
-        folders = Pathname.new(PATTERNSLIB_PAT_PATH).children.select { |c| c.directory? }
-        folders.each() do |path|
-          pattern = path.sub(/^#{PATTERNSLIB_PAT_PATH}\//, '')
-
-          Dir.foreach(path) do |item|
-            next if item == '.' or item == '..'
-            original = "#{path}/#{item}"
-            if not File.file? original
-              next
-            end
-            basename = File.basename item
-            if basename.end_with?(".sw?") or basename.end_with?("~") or basename.start_with?(".")
-              next
-            end
-            new = "#{COPIED_DOCS_PATH}/patterns/#{pattern}/#{basename}"
-            diff = Diffy::Diff.new(new, original, :source => 'files').to_s(:text)
-            if not diff.empty?
-              dirty = true
-              puts "\nFile #{new} is out of sync, will be fixed now."
-            end
-          end
-        end
-      end
-      if (not Dir.exist?(COPIED_DOCS_PATH)) or dirty
-        FileUtils.rm_rf(COPIED_DOCS_PATH)
-        FileUtils.cp_r(PATTERNSLIB_DOCS_PATH, COPIED_DOCS_PATH)
-        puts "Copying over the documentation files from Patternslib"
-      end
-    end
 
     def create_static_files(site, path, pattern_name)
       # Create Jekyll:StaticFile instances for all files that need to be copied
@@ -60,7 +24,7 @@ module Jekyll
         else
           rel_path = item_path.sub(PATTERNSLIB_PAT_PATH, "#{COPIED_DOCS_PATH}/patterns")
           rel_path = rel_path.sub(item, '')
-          site.static_files << Jekyll::StaticFile.new(site, site.source, rel_path, item)
+          site.static_files << Jekyll::StaticFile.new(site, "#{site.source}/patternslib", rel_path, item)
         end
       end
     end
@@ -68,7 +32,7 @@ module Jekyll
     def generate_docs(site)
       # Make sure that the canonical docs (e.g. from Patternslib) are available
       # in the _site directory.
-      self.copy_docs_if_necessary()
+      # self.copy_docs_if_necessary()
       folders = Pathname.new(PATTERNSLIB_PAT_PATH).children.select { |c| c.directory? }
       folders.each() do |path|
         pattern_name = path.sub(/^#{PATTERNSLIB_PAT_PATH}/, '')
