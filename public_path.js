41fe125a3aab21ca3a8dd1aeeac4c93a92f59a7d
Johannes Raggam <thetetet@gmail.com>
2020-12-05 14:58:12 +0100

    Update for new Patternslib/ES6 structure

---
+++ public_path.js
@@ -0,0 +1,4 @@
+// NOTE: Import this file before any other files
+// Overwrite path to load resources or use default one.
+__webpack_public_path__ =
+  window.__patternslib_public_path__ || "/assets/script/"; // eslint-disable-line no-undef
